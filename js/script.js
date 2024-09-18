bf2f936d3dfff15e47d69afeb87a5f929b61435a
Francisco Díaz <85258899+FranciscoDiaz01@users.noreply.github.com>
2024-09-18 17:39:34 -0300

    Add files via upload
    Se crea proyecto Portfolio del programa ONE de Oracle, Alura Latam.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* let boton = document.querySelector("#boton__linkedin");
+boton.addEventListener("click", function () {
+  (window.open = "https://www.linkedin.com/in/francisco-diaz-robledo/");
+});
+ */
+function git(url) {
+  let win = window.open("https://github.com/FranciscoDiaz01", "_blank");
+}
+
+function link(url) {
+  let win = window.open(
+    "https://www.linkedin.com/in/francisco-diaz-robledo/",
+    "_blank"
+  );
+}
